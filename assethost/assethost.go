7e6c1f3cd3cf87f3ea87915bc49f84a642432336
spaceey1 <bruno@gadula.ca>
2025-04-05 13:59:47 +0200

    Basic auth for items

--- assethost/assethost.go
+++ assethost/assethost.go
@@ -1,8 +1,41 @@
 package assethost
 
-import "net/http"
+import (
+	"io"
+	"net/http"
+	"resonite-file-provider/authentication"
+	"resonite-file-provider/database"
+	"strings"
+)
+func isOwnedBy(owner string, url string) bool {
+	var exists bool
+	database.Db.QueryRow("SELECT EXISTS (SELECT 1 from Users where username = ? AND id = (SELECT user_id FROM users_inventories WHERE inventory_id = (SELECT inventory_id FROM Folders WHERE id = (SELECT folder_id FROM `Items` WHERE url = ?))))", owner, url).Scan(&exists)
+	return exists
+}
+func handleRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+		claims, err := authentication.ParseToken(strings.Split(string(body), "\n")[0])
+		if err != nil {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+		username := claims.Username
+		if !isOwnedBy(username, r.URL.Path) {
+			http.Error(w, "Forbidden", http.StatusForbidden)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
 
 func StartHost(){
-	http.Handle("/", http.FileServer(http.Dir("./assets")))
+	fs := http.FileServer(http.Dir("./items"))
+	http.Handle("/items/", handleRequest(fs))
+	http.Handle("/assets/", handleRequest(http.FileServer(http.Dir("./assets"))))
 	http.ListenAndServe(":8080", nil)
 }
